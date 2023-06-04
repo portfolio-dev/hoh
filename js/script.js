43103671b07368d396208889f7116e84ccbf0c76
Meldir <134066210+portfolio-dev@users.noreply.github.com>
2023-06-05 00:32:59 +0700

    add js folder and file

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// title bergerak
+var titleText = "House of Heart - Rumah Singgah Pasien Jantung Anak  ";
+        var speed = 100; // Kecepatan perubahan (ms)
+        var i = 0;
+
+        function animateTitle() {
+            document.title = titleText.substring(i) + titleText.substring(0, i);
+            i++;
+            if (i > titleText.length) {
+                i = 0;
+            }
+            setTimeout(animateTitle, speed);
+        }
+
+        animateTitle();
+
+// menu-toggler tertutup otomatis
+var menuToggle = document.getElementById('menu-toggler');
+var menuContent = document.querySelector('.all-links');
+var menuItems = document.querySelectorAll('.all-links li');
+
+menuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        menuToggle.checked = false;
+    });
+});
+
+
+
