099582acd8dbf0126318a92bae36eff3460e417e
Meldir <134066210+portfolio-dev@users.noreply.github.com>
2023-06-07 22:42:31 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,14 +19,67 @@ setInterval(mv, 1000);
 
 // menu-toggler tertutup otomatis
 function mt() {
-var menuToggle = document.getElementById('menu-toggler');
-var menuContent = document.querySelector('.all-links');
-var menuItems = document.querySelectorAll('.all-links li');
+    var menuToggle = document.getElementById('menu-toggler');
+    var menuContent = document.querySelector('.all-links');
+    var menuItems = document.querySelectorAll('.all-links li');
 
-menuItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-        menuToggle.checked = false;
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            menuToggle.checked = false;
+        });
     });
-});
 };
 setInterval(mt,1000);
+
+// redirect link
+function home() {
+    window.location.href ="#home"
+    event.preventDefault();
+};
+function about() {
+    window.location.href ="#about"
+    event.preventDefault();
+};
+function dailyNeeds() {
+    window.location.href ="#services"
+    event.preventDefault();
+};
+function assistance() {
+    window.location.href ="#services"
+    event.preventDefault();
+};
+function icuSupport() {
+    window.location.href ="#services"
+    event.preventDefault();
+};
+function consultation() {
+    window.location.href ="#services"
+    event.preventDefault();
+};
+function facilities() {
+    window.location.href ="#facilities"
+    event.preventDefault();
+};
+function myPrayers() {
+    window.location.href ="#"
+    event.preventDefault();
+};
+function healed() {
+    window.location.href ="#"
+    event.preventDefault();
+};
+function rip() {
+    window.location.href ="#"
+    event.preventDefault();
+};
+function finance() {
+    window.location.href ="#"
+    event.preventDefault();
+};
+function donate() {
+    window.location.href ="#contact"
+    event.preventDefault();
+};
+function igLink() {
+    window.open("https://www.instagram.com/houseofheart.rumahsinggah/", "target");
+};
