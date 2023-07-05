d2eb0f5f700cc63eaf5bc2297430709b44e210c9
Meldir <134066210+portfolio-dev@users.noreply.github.com>
2023-07-06 00:16:05 +0700

    Add files via upload

--- js/account.js
+++ js/account.js
@@ -2,6 +2,7 @@ var loginImg = document.querySelectorAll('.login-img');
 var loginForm = document.querySelectorAll('.login-form');
 var regisImg = document.querySelectorAll('.regis-img');
 var regisForm = document.querySelectorAll('.regis-form');
+var resetForm = document.querySelectorAll('.reset-form');
   
 function regisAccount() {
     loginImg.forEach(function(element) {
@@ -24,6 +25,24 @@ function regisAccount() {
     });
     loginForm.forEach(function(element) {
         element.style.display = 'block';
+    });
+    regisImg.forEach(function(element) {
+        element.style.display = 'none';
+    });
+    regisForm.forEach(function(element) {
+        element.style.display = 'none';
+    });
+}
+
+function resetAccount() {
+    loginImg.forEach(function(element) {
+        element.style.display = 'block';
+    });
+    resetForm.forEach(function(element) {
+        element.style.display = 'block';
+    });
+    loginForm.forEach(function(element) {
+        element.style.display = 'none';
     });
     regisImg.forEach(function(element) {
         element.style.display = 'none';
